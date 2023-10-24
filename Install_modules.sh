c36ccdb2ae0a2d1b36bac754550aef748de0dcfd
vsc44685 vscuser <vsc44685@gligar07.gastly.os>
2023-10-24 14:57:55 +0200

    add Install modules

---
+++ Install_modules.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Create the output folder if it doesn't exist
+mkdir -p "./modules"
+# Define the target folder for Singularity images
+output_folder="./modules"
+# List of Docker images to download
+docker_images=(
+    "docker://quay.io/biocontainers/cutadapt:4.5--py39hf95cd2a_0"
+    "docker://quay.io/biocontainers/fastqc:0.11.9--0"
+    "docker://quay.io/biocontainers/subread:2.0.1--hed695b0_0"
+    "docker://quay.io/biocontainers/samtools:1.2--0"
+    "docker://parisepigenetics/umitools:latest"
+    "docker://quay.io/biocontainers/seqtk:1.4--he4a0461_1"
+    "docker://quay.io/biocontainers/star:2.7.8a--0"
+)
+
+# Loop through the list and convert each Docker image to Singularity
+for image in "${docker_images[@]}"; do
+  # Extract the image name (last part) to use as the Singularity image name
+  image_name=$(echo "$image" | awk -F'/' '{print $NF}')
+  # Pull the Docker image and convert it to Singularity
+  singularity pull "$output_folder/$image_name.sif" "$image"
+done
+
+echo "Singularity images downloaded and saved to $output_folder"
